3a1210e722b03a6a5fc699b756cde2c567877155
Anderson <a25216358@alumnos.uady.mx>
2026-04-16 17:09:29 -0600

    Update __init__.py

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,6 @@
-# ==========================================
+
 # Módulo de Datos
-# ==========================================
+
 # Aquí manejamos todo lo relacionado con datos:
 # - Cargar datos de criptomonedas
 # - Importar métricas energéticas
